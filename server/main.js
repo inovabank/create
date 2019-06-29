6e59bb0290e7c0362e465516ba32924063a596b7
rubens <rubens.miguek@gmail.com>
2019-06-29 15:44:47 -0300

    create user method

--- server/main.js
+++ server/main.js
@@ -1,6 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
+  Accounts.onCreateUser((options, user) => {
+    user.profile = options.profile;
+    user = Object.assign({
+      whatsapp: user.username,
+    }, user)
+    delete user['username'];
+    return user;
+  });
+
   Meteor.methods({
     'APICall': (method, url, options) => {
       HTTP.call(method, url, options, function(error, result) {
